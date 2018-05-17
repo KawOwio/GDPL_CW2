3eaf607700d6304ddba2fe42a7a994cade39bec3
Nikita Gribuska <s5065617@bournemouth.ac.uk>
2018-05-17 19:26:36 +0100

    Initial commit.

---
+++ Assets/Fan.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    public int JUMP_HEIGHT;
+    public Vector3 trans;
+    private int velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (velocity > 0)
+        {
+            transform.Translate(trans * Time.deltaTime, Space.World);
+            velocity--;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.name == "Bounce")
+        {
+            velocity = JUMP_HEIGHT;
+
+            Debug.Log(other.gameObject.name);
+        }
+    }
+}
